4b88fabb02c637c639a52ebb5bd7eb2cdccc8699
Iqra Sarwar <141229997+IqraSarwarArabi@users.noreply.github.com>
2023-08-03 18:03:46 +0500

    variables

---
+++ basics/variables.php
@@ -0,0 +1,21 @@
+<!-- 
+Always start with $.
+Can use underscore (_) and alphanumerics in name.
+Case - senstivive
+Can change the datatype randomly.
+Use camelCase
+can't start name with an integer
+-->
+
+<?php
+$Name = "iqrasarwar";
+$name = "IqraSarwar";
+$name1 = "iqraSarwar1";
+$n12n = "rolllno";
+$name1_name2 = "iqra_sarwar";
+
+echo nl2br("\$name= $name
+\$Name= $Name
+\$n12n= $n12n
+\$name1_name2= $name1_name2");
+?>
