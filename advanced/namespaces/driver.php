fd79f3b99f7128f67bc258cdfabb71fb4064262b
Iqra Sarwar <141229997+IqraSarwarArabi@users.noreply.github.com>
2023-08-04 14:21:35 +0500

    namespaces

---
+++ advanced/namespaces/driver.php
@@ -0,0 +1,21 @@
+<?php
+use MyApp\Math\Calculator;
+use MyApp\Geometry\Circle;
+use MyApp\Geometry\Square;
+
+// Math namespace
+$sum = Calculator::add(5, 3);
+echo "Sum: $sum<br>"; // Output: Sum: 8
+
+$difference = Calculator::subtract(10, 4);
+echo "Difference: $difference<br>"; // Output: Difference: 6
+
+// Geometry namespace
+$circle = new Circle(5);
+$circleArea = $circle->calculateArea();
+echo "Circle Area: $circleArea<br>"; // Output: Circle Area: 78.539816339745
+
+$square = new Square(4);
+$squareArea = $square->calculateArea();
+echo "Square Area: $squareArea<br>"; // Output: Square Area: 16
+?>
---
+++ advanced/namespaces/math.php
@@ -0,0 +1,13 @@
+<?php
+namespace MyApp\Math {
+    class Calculator {
+        public static function add($a, $b) {
+            return $a + $b;
+        }
+
+        public static function subtract($a, $b) {
+            return $a - $b;
+        }
+    }
+}
+?>
